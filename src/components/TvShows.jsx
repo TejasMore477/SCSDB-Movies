d5eaf514d2daf81b162dce5096f29610430f1546
Tejas More <tejasmore660@gmail.com>
2025-04-17 11:36:14 +0530

    created Tv shows compoent responsive

--- src/components/TvShows.jsx
+++ src/components/TvShows.jsx
@@ -49,23 +49,24 @@ function TvShows() {
 
   return tvShows.length > 0 ? (
     <div className="pt-5 w-full overflow-x-hidden">
-      <div className="w-full flex items-center justify-between px-5">
-        
-        <div className="flex w-fit items-center gap-5 text-2xl">
-          <i
-            onClick={() => navigate(-1)}
-            className="text-[#6556CD] ri-arrow-left-line active:text-[#503ecb] cursor-pointer"
-          ></i>
-          <h1 className="text-zinc-400 font-semibold">Tv Shows</h1>
-        </div>
 
-        <div className="w-[90%] flex items-center gap-5">
+      <div className="flex items-center gap-5 text-2xl px-5">
+        <i
+          onClick={() => navigate(-1)}
+          className="text-[#6556CD] ri-arrow-left-line active:text-[#503ecb] cursor-pointer"
+        ></i>
+        <h1 className="text-zinc-400 font-semibold">Tv Shows</h1>
+      </div>
 
-          <div className="w-full">
-            <TopNav />
+      <div className="w-full flex items-center justify-end px-8 mt-3">
+        <div className="w-[100%] lg:flex items-center gap-5">
+          <div className="xl:w-[50%] lg:w-[80%] w-full flex items-end md:items-start">
+            <TopNav justify={"justify-start"} />
           </div>
 
-          <DropDown
+          <div className="xl:w-auto lg:w-[40%] flex sm:justify-end justify-center items-center gap-5 lg:pt-0 pt-2 pr-10">
+            <div className="xl:w-full lg:w-[30%] md:w-[20%] w-[30%]">
+            <DropDown
             title={"Catogories"}
             options={["popular","top_rated","on_the_air", "airing_today"]}
             handleChange={(e) => {
@@ -73,9 +74,9 @@ function TvShows() {
             }}
             icon={<i className="ri-apps-2-add-line"></i>}
           />
-
+            </div>
+          </div>
         </div>
-
       </div>
 
       <InfiniteScroll
