31ace9ceaad84d46eab0b45960050ce2cd05d5ad
Tejas More <tejasmore660@gmail.com>
2025-04-21 16:26:35 +0530

    removing color stylings

--- src/components/Trendings.jsx
+++ src/components/Trendings.jsx
@@ -56,7 +56,7 @@ function Trendings() {
   return trending.length > 0 ? (
     <div className="pt-5 w-full overflow-x-hidden">
 
-      <div className="flex items-center gap-5 text-2xl lg:px-8 px-5 xl:bg-red-900 lg:bg-fuchsia-900 md:bg-green-900 sm:bg-orange-900">
+      <div className="flex items-center gap-5 text-2xl lg:px-8 px-5">
         <i
           onClick={() => navigate(-1)}
           className="text-[#6556CD] ri-arrow-left-line active:text-[#503ecb] cursor-pointer"
@@ -64,15 +64,15 @@ function Trendings() {
         <h1 className="text-zinc-400 font-semibold">Trending</h1>
       </div>
 
-      <div className="w-full flex items-center justify-end lg:px-8 px-5 mt-3 xl:bg-red-9000 lg:bg-fuchsia-9000 md:bg-green-9000 sm:bg-orange-9000">
+      <div className="w-full flex items-center justify-end lg:px-8 px-5 mt-3">
         <div className="w-[100%] lg:flex items-center gap-5">
 
-            <div className="xl:w-[50%] lg:w-[80%] w-full flex items-end md:items-start bg-red-3000">
+            <div className="xl:w-[50%] lg:w-[80%] w-full flex items-end md:items-start">
             <TopNav justify={"justify-start"}/>
             </div>
 
-          <div className="xl:w-auto lg:w-[40%] flex sm:justify-end justify-center items-center gap-5 bg-yellow-2000 lg:pt-0 pt-2">
-            <div className="xl:w-full lg:w-[30%] md:w-[20%] w-[30%] bg-yellow-6000 ">
+          <div className="xl:w-auto lg:w-[40%] flex sm:justify-end justify-center items-center gap-5 lg:pt-0 pt-2">
+            <div className="xl:w-full lg:w-[30%] md:w-[20%] w-[30%]">
             <DropDown
             title={"Catogories"}
             options={["tv", "movie", "all"]}
@@ -83,7 +83,7 @@ function Trendings() {
           />
             </div>
 
-            <div className="xl:w-full lg:w-[30%] md:w-[20%] w-[30%] bg-yellow-6000">
+            <div className="xl:w-full lg:w-[30%] md:w-[20%] w-[30%]">
             <DropDown
             title={"Duration"}
             options={["day", "week"]}
