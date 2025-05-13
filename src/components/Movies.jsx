bf2072006879944c26dd53dfb0d44958523125c4
Tejas More <tejasmore660@gmail.com>
2025-05-13 18:22:45 +0530

    styles(Movies):responsive

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -54,7 +54,7 @@ function Movies() {
           onClick={() => navigate(-1)}
           className="text-[#6556CD] ri-arrow-left-line active:text-[#503ecb] cursor-pointer"
         ></i>
-        <h1 className="text-zinc-400 font-semibold">Movies</h1>
+        <h1 className="text-zinc-400 font-semibold sm:text-xl text-lg">Movies</h1>
       </div>
 
       <div className="w-full flex items-center justify-end px-8 mt-3">
