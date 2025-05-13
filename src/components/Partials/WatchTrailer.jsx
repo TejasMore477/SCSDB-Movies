5311503b026d33fc42efdf5b1fef6b82283bfe9c
Tejas More <tejasmore660@gmail.com>
2025-05-13 18:25:40 +0530

    styles(watchrailer):responsive

--- src/components/Partials/WatchTrailer.jsx
+++ src/components/Partials/WatchTrailer.jsx
@@ -5,7 +5,7 @@ function WatchTrailer({ pathname }) {
   return (
     <Link
       to={`${pathname}/trailer`}
-      className="px-4 py-2 mb-2 rounded-md bg-[#6556CD] mt-5 hover:bg-[#4b3abc] active:bg-[#4f3bd0]"
+      className="px-4 py-2 mb-2 text-sm sm:text-base rounded-md bg-[#6556CD] mt-5 hover:bg-[#4b3abc] active:bg-[#4f3bd0]"
     >
       WatchTrailer
     </Link>
